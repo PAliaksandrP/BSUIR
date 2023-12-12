72fe19b61ca43e617ac6b1cf595dd713f710da8c
PAliaksandrP <90904058+PAliaksandrP@users.noreply.github.com>
2023-12-13 00:02:19 +0300

    Delete OOP/lab2/visitor.cpp

--- OOP/lab2/visitor.cpp
+++
@@ -1,8 +0,0 @@
-#include "visitor.h"
-
-
-Visitor::Visitor(string last_name, string first_name, bool sex, bool clothes_capacity, string location, Hotel hotel,Hotel_room room,int room_number):Person(last_name, first_name, sex, clothes_capacity, location,  hotel) {
-	this->room_ = room;
-	this->room_number_ = room_number;
-}
-Visitor::Visitor(){}
