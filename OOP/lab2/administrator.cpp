e83aa2a20eab0f0413b0628115d3d24494110d15
PAliaksandrP <90904058+PAliaksandrP@users.noreply.github.com>
2023-12-13 00:01:46 +0300

    Delete OOP/lab2/administrator.cpp

--- OOP/lab2/administrator.cpp
+++
@@ -1,8 +0,0 @@
-#include "administrator.h"
-
-Administrator::Administrator(string last_name, string first_name, bool sex, bool clothes_capacity, string location, Hotel hotel, string rang) :Person(last_name, first_name, sex, clothes_capacity, location, hotel) {
-	this->rang_ = rang;
-}
-bool Administrator::invite(Hotel_room room, Visitor visitor, int number) {
-
-}
